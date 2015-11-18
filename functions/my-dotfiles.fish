9d1f3e95327735c8381a98f449d30cb21115ae78
Ben Kutil <ktb@mts-mp-beug.makethin.gs>
2015-11-18 15:34:21 -0500

    :zap: add boilerplate oh-my-fish package

---
+++ functions/my-dotfiles.fish
@@ -0,0 +1,10 @@
+# SYNOPSIS
+#   my-dotfiles [options]
+#
+# USAGE
+#   Options
+#
+
+function my-dotfiles -d "My package"
+end
+
---
+++ uninstall.fish
@@ -0,0 +1,3 @@
+function uninstall --on-event uninstall_my-dotfiles
+end
+
